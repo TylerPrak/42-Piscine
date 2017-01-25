0dcef345488916d16cfa1b5e722d92319fee5772
tyler <prayforparasites@gmail.com>
2017-01-25 17:34:24 -0500

    Working version of aff_last_param.c

--- Level-0/aff_last_param.c
+++ Level-0/aff_last_param.c
@@ -1,10 +1,24 @@
 #include <unistd.h>
 
-void	main (int argc, char **argv[])
+void	ft_putchar (char c)
+{
+	write (1, &c, 1);
+}
+
+void	main (int argc, char **argv)
 {
 	int i = 0;
-	if ( argc == 1)
-	{
+	int ac = argc - 1;
+
+	if (argc == 1)
 		write (1, "\n", 1);
+	else
+	{
+		while (argv[ac][i] != '\0')
+		{
+			ft_putchar(argv[ac][i]);
+			i++; 
+		}	
+		ft_putchar ('\n');
 	}
 }
